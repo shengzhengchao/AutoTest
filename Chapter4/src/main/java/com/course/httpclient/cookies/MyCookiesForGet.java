bbd6574319b820d3e971bcdfed2925669a18f246
shenzhengchao <shenchaoc6@126.com>
2024-12-09 19:38:45 +0800

    sprintboot基础学习

--- Chapter4/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter4/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -7,8 +7,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
@@ -19,6 +17,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+//运行moco环境命令: java -jar ./Chapter3/moco-runner-0.11.0-standalone.jar http -p 8888 -c ./Chapter3/startupWithCookies.json
+
 public class MyCookiesForGet {
     private String url;
     private ResourceBundle bundle;
@@ -27,6 +27,7 @@ public class MyCookiesForGet {
 
     @BeforeTest
     public void beforeTest(){
+
         context = HttpClientContext.create();
         bundle = ResourceBundle.getBundle("application", Locale.CHINA);
         url = bundle.getString("test.url");
@@ -53,9 +54,35 @@ public class MyCookiesForGet {
         for (Cookie cookie : cookieList) {
             String name = cookie.getName();
             String value = cookie.getValue();
+            String domain = cookie.getDomain();
+            String path = cookie.getPath();
             System.out.println("cookieName: " + name
-            + ";\ncookieValue: " + value);
+            + ";\ncookieValue: " + value
+                    + ";\ncookieDomain: " + domain
+                    + ";\ncookiePath: " + path);
         }
+    }
 
+    //此方法依赖于方法testGetCookies()的执行
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies() throws IOException {
+        String uri = bundle.getString("test.get.with.cookies");
+        String testUrl = this.url + uri;
+
+        HttpGet get = new HttpGet(testUrl);
+        //设置cookies信息
+        HttpClient client = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+        HttpResponse response = client.execute(get);
+
+        //获取响应的状态码
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("statusCode = " + statusCode);
+
+        if(statusCode == 200) {
+            String result = EntityUtils.toString(response.getEntity(), "utf-8");
+            System.out.println(result);
+        }else {
+            System.out.println("响应失败!!!");
+        }
     }
 }
